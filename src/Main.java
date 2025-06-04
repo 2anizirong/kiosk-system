248b878c01b845de55e80e11792b6bae2f3fa143
ian <hope030327@naver.com>
2025-06-04 20:49:55 +0900

    Feat: paymentë, userPanel 세부 클래스 추가 (추후 ì 리펙토링 필요..)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import payment.CardPayment;
+import userPanelSystem.Customer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Customer customer = new Customer();
+        customer.selectLanguage(); // 언어 선택
+        customer.selectOrder();    // 주문 및 결제 흐름
     }
 }
--- src/payment/PaymentService.java
+++ src/payment/PaymentService.java
@@ -1,5 +1,21 @@
-package Pay;
+package payment;
 
-public class CardPay {
-    
+public class PaymentService {
+
+    // 결제 승인 처리
+    public boolean paymentConfirmation(String cardNumber, int amount) {
+        // 실제 카드 결제 승인 로직이 들어갈 부분
+        if (cardNumber.length() == 16 && amount > 0) {
+            System.out.println("[PaymentService] 결제 승인 완료");
+            return true;
+        } else {
+            System.out.println("[PaymentService] 결제 승인 실패");
+            return false;
+        }
+    }
+
+    // 결제 내역 저장 (예시)
+    public void savePaymentHistory(String cardNumber, int amount) {
+        System.out.println("[PaymentService] 결제 내역 저장: 카드 " + cardNumber + ", 금액 " + amount + "원");
+    }
 }
