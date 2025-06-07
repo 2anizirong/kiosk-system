3745ce5b57c009ddc67ac9e7283a8ba4a2d6af69
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2025-06-07 13:36:50 +0900

    카트 리스트 타입 변경

--- src/view/CartPanel.java
+++ src/view/CartPanel.java
@@ -1,6 +1,6 @@
 package view;
 
-import cart.Cart;
+import menu.Menu;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,7 +8,7 @@ import java.awt.*;
 import java.util.List;
 
 public class CartPanel extends JPanel {
-    public CartPanel(KioskFrame frame, Cart cart) {
+    public CartPanel(KioskFrame frame) {
         setLayout(new BorderLayout());
 
         JLabel title = new JLabel("장바구니", SwingConstants.CENTER);
@@ -19,11 +19,11 @@ public class CartPanel extends JPanel {
         JPanel cartItemPanel = new JPanel();
         cartItemPanel.setLayout(new BoxLayout(cartItemPanel, BoxLayout.Y_AXIS));
 
-        List<String> cartItems = cart.returnCartItemList();
+        List<Menu> cartItems = cart.returnCartItemList();
         if (cartItems.isEmpty()) {
             cartItemPanel.add(new JLabel("장바구니가 비어 있습니다."));
         } else {
-            for (String item : cartItems) {
+            for (Menu item : cartItems) {
                 JLabel itemLabel = new JLabel("• " + item);
                 itemLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
                 itemLabel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
