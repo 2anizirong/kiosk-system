22058c58d4ae2dd984d3c8e2f25ead5caf147358
yyen <brenda1217@naver.com>
2025-06-07 13:35:24 +0900

    cart 경로 수정

---
+++ src/cart/Cart.java
@@ -0,0 +1,29 @@
+package cart;
+
+import java.util.ArrayList;
+import java.util.List;
+import menu.Menu; // Item 클래스가 menu 패키지에 있다고 가정
+
+public class Cart {
+    private List<Menu> cartItems = new ArrayList<Menu>();
+
+    public Cart() {
+    }
+
+    void addToCart(Menu menu){
+        cartItems.add(menu);
+        System.out.println(menu.getName() + " 이(가) 장바구니에 추가되었습니다.");
+    }
+
+    int returnTotalPrice() {
+        int total = 0;
+        for (Menu item : cartItems) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    List<Menu> returnCartItemList(){
+            return new ArrayList<>(cartItems);
+    }
+}
